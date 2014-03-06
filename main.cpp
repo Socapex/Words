04dbc82567c9badca5c1bf6f02d28b69990ffe45
alexarse <arsemusic@live.com>
2014-03-06 11:54:38 -0500

    Delete commented code in main.

--- main.cpp
+++ main.cpp
@@ -13,19 +13,8 @@ int main(int argc, char* argv[])
 	CMap* m = cmain.GetMap();
 	m->Print(DEBUG[D_OUT1]);
 
-	// Refaire cette routine plus simplement dans CMap::GetWordAttribute("...").
-	//---------------------------------------
-	//CAtt<unsigned int> att_markov;
-	///att_markov.SetValue(4);
-	///CWord* w = m->GetWordPtr();
-	///w->AddAttribute("Markov", &att_markov);
-
-	//CWord* w2 = m->GetWordPtr();
-	//CAttribute* att2 = (*w2)["Markov"];
-	//int value = ((CAtt<unsigned int>*)att2)->GetValue();
-	ATT_MARKOV_T* markov = (ATT_MARKOV_T*)m->GetWordAttribute("Markov");
+    ATT_MARKOV_T* markov = (ATT_MARKOV_T*)m->GetWordAttribute("Markov");
 	DEBUG[D_OUT2] << "ATTRIBUTE MARKOV : " << markov->GetValue() << endl;
-	//---------------------------------------
 	
 	//CDataBase data_base(mymap);
 	//data_base.SaveData("cdb");
